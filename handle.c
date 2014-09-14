3d6d67293b6ec90e9d343e59b75a6a9b1277e6ab
Terry Liang <liang810612@hotmail.com>
2014-09-14 15:01:25 -0500

    handle finish

--- handle.c
+++ handle.c
@@ -7,6 +7,31 @@
 #include <unistd.h>
 #include "util.h"
 
+/*
+ * signal handler
+ * catch SIGINT and SIGUSR1 and print different messages
+ */
+
+void handler(int sig){
+	if(sig == SIGINT){
+		ssize_t bytes; 
+		const int STDOUT = 1; 
+		bytes = write(STDOUT, "Nice try.\n", 10); 
+		if(bytes != 10) 
+	   		exit(-999);
+	}
+	if(sig == SIGUSR1){
+		ssize_t bytes; 
+		const int STDOUT = 1; 
+		bytes = write(STDOUT, "exiting.\n", 10); 
+		if(bytes != 10) 
+   			exit(-999);
+   		exit(1);
+	}
+}
+
+
+
 
 /*
  * First, print out the process ID of this process.
@@ -20,6 +45,15 @@
 int main(int argc, char **argv)
 {
 	printf("Process ID: %d\n", (int)getpid());
+	signal(SIGINT, handler);
+	signal(SIGUSR1, handler);
+
+	struct timespec ts;
+	ts.tv_sec = 1;
+	while(1){
+		nanosleep(&ts, NULL); 
+			printf("still here\n");
+	}
   return 0;
 }
 
